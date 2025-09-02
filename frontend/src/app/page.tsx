8dcd0d9837525aeae718d217a7a1eb5ce1670e6a
w1zll <filin.vitalik2244@mail.ru>
2025-09-02 19:26:21 +0500

    frontend set containers

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-   return <div>mini-blog</div>;
+   return (
+      <>
+         <h1 className="text-3xl text-center mt-12">Добро пожаловать на Mini blog</h1>
+      </>
+   );
 }
